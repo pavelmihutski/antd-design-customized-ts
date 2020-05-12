e1b795c4327746f9a928338c9bab9b34a8d706c4
pavelmihutski <pavelmihutski@gmail.com>
2020-05-12 10:31:11 +0300

    theming init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+		<div>
+			<Button type="primary">Primary</Button>
+			<Button>Default</Button>
+			<Button type="dashed">Dashed</Button>
+		</div>
+		<br />
+		<div>
+			<Button type="primary" size="large">
+				Primary
+			</Button>
+			<Button size="large">Default</Button>
+			<Button size="large" type="dashed">
+				Dashed
+			</Button>
+		</div>
+
+		<br />
+		<div>
+			<Button type="primary" size="small">
+				Primary
+			</Button>
+			<Button size="small">Default</Button>
+			<Button size="small" type="dashed">
+				Dashed
+			</Button>
+		</div>
+
+	</div>
   );
 }
 
